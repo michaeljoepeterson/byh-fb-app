9b84e1bf7b1ff9e0fd7f23940e57f17333c48982
michael peterson <michaeljoepeterson@gmail.com>
2021-03-29 20:22:31 -0600

    fix bug with columns not matching up if field missing

--- src/components/sub-components/form-table.js
+++ src/components/sub-components/form-table.js
@@ -10,6 +10,8 @@ import './styles/form-table.css';
 
 
 function FormTable(props){
+    //map of field id to column data
+    
     //const [formData,setFormData] = useState(null);
     let formData = props.forms ? [...props.forms] : [];
     formData = formData.map(form => {
@@ -23,55 +25,108 @@ function FormTable(props){
         });
 
         return form;
-    })
+    });
+
+    const buildColumnMap = () => {
+        let colMap = [];
+        for(let i = 0;i < formData.length;i++){
+            //only build from first 20 results
+            //to do return possible cols from service
+            if(i === 20){
+                break;
+            }
+            formData[i].fields.forEach((field,index) => {
+                let foundField = colMap.find(col => col.id === field.id);
+                if(!foundField){
+                    colMap.push({
+                        id:field.id,
+                        title:field.title
+                    });
+                }
+            });
+        }
+        colMap.sort((a,b) => {
+            if(a.title > b.title){
+                return 1;
+            }
+            else{
+                return -1;
+            }
+        })
+        return colMap;
+    }
+
+    const buildHeaders = (cols) => {
+        if(cols){
+            let headers = cols.map(field => {
+                try{
+                    if(field && field.title){
+                        return(<TableCell key={field.title}>{field.title}</TableCell>
+                        );
+                    }
+                    return null;
+                }
+                catch(e){
+                    console.warn('Error builing headers: ', e);
+                    return null;
+                }
+            })
+
+            return headers;
+        }
+
+        return null;
+    }
+
+    const buildColumnData = (colMap) => {
+        let cols = formData.map(form => {
+            return (
+            <TableRow>
+                {
+                    colMap.map((col,index) => {
+                        try{
+                            let foundField = form.fields.find(field => field.id === col.id);
+                            if(foundField){
+                                if(foundField.type && foundField.type.toLowerCase().trim() === 'date'){
+                                    let dateValue = new Date(foundField.value);
+                                    return <TableCell key={foundField.id + foundField.value}>{dateValue.toDateString()}</TableCell>
+                                }
+                                else{
+                                    return (
+                                    <TableCell key={foundField.id + foundField.value}>{foundField.value}</TableCell>
+                                    )
+                                }
+                            }
+                            else{
+                                return (<TableCell key={col.id + index}></TableCell>)
+                            }
+                        }
+                        catch(e){
+                            console.warn('error building cols',e);
+                            return (<TableCell key={col.id + index}></TableCell>)
+                        }
+                    })
+                }
+            </TableRow>
+            );
+        });
+
+        return cols;
+
+    }
+
+    let columnMap = formData && formData.length > 0 ? buildColumnMap() : [];
 
     return(
         <TableContainer className="form-table-container" component={Paper}>
             <Table stickyHeader >
                 <TableHead>
                     <TableRow>
-                        {
-                            formData.length > 0 ? formData[0].fields.map(field => {
-                                try{
-                                    if(field && field.title){
-                                        return(<TableCell key={field.title}>{field.title}</TableCell>
-                                        );
-                                    }
-                                    return null;
-                                }
-                                catch(e){
-                                    console.warn('Error builing headers: ', e);
-                                    return null;
-                                }
-                            }): 
-                            null
-                        }
+                        {buildHeaders(columnMap)}
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {
-                        formData.length > 0 ? 
-                        formData.map((form,i) => {
-                            return (
-                                <TableRow key={i}>
-                                    {
-                                    form.fields.map(field => {
-                                        if(field.type && field.type.toLowerCase().trim() === 'date'){
-                                            let dateValue = new Date(field.value);
-                                            return <TableCell key={field.id + field.value}>{dateValue.toDateString()}</TableCell>
-                                        }
-                                        else{
-                                            return (
-                                            <TableCell key={field.id + field.value}>{field.value}</TableCell>
-                                            )
-                                        }
-                                    })
-                                    }
-                                </TableRow>
-                            );
-                        })
-                        : null
-                    }
+                    {buildColumnData(columnMap)}
                 </TableBody>
             </Table>
         </TableContainer>
